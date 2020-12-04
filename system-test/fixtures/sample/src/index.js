beae24a4d06848644c35a1856b625fbd580ea64b
Sofia Leon <sofialeon@google.com>
2020-12-03 20:06:18 -0800

    feat!: samples

--- system-test/fixtures/sample/src/index.js
+++ system-test/fixtures/sample/src/index.js
@@ -17,7 +17,7 @@
 // ** All changes to this file may be overwritten. **
 
 /* eslint-disable node/no-missing-require, no-unused-vars */
-const recommendationengine = require('recommendationengine');
+const recommendationengine = require('@google-cloud/recommendation-engine');
 
 function main() {
   const catalogServiceClient = new recommendationengine.CatalogServiceClient();
